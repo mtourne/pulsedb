bb8fcc0569af919eaaae5b419db1dcb005e81a8e
Max Lapshin <max@maxidoors.ru>
2014-01-21 15:55:45 +0400

    fixed webhandler to be able to launch with key

--- simple_webhandler.erl
+++ simple_webhandler.erl
@@ -1,12 +1,32 @@
-#!/usr/bin/env ERL_LIBS=.. escript
-
+#!/usr/bin/env escript
 %%
 %%! -env ERL_LIBS .. -pa ebin
 
 -mode(compile).
 
-main([Port_, Path]) ->
+main([]) ->
+  io:format("simple_webhandler.erl port path_to_database [auth-key-of-16-byte-length]\n"),
+  halt(1);
+
+main(Argv) ->
+  process_flag(trap_exit,true),
+  try main0(Argv)
+  catch
+    C:E ->
+      io:format("error in boot: ~p:~p\n~p\n", [C,E,erlang:get_stacktrace()]),
+      halt(2)
+  end.
+
+main0([Port_, Path|Args]) ->
   Port = list_to_integer(Port_),
+
+  Auth = case Args of
+    [Key|_] when length(Key) == 16 ->
+      NetAuth = [{key,iolist_to_binary(Key)}],
+      [{auth,pulsedb_netpush_auth,NetAuth}];
+    [] ->
+      []
+  end,
 
   application:start(crypto),
   application:start(asn1),
@@ -21,9 +41,12 @@ main([Port_, Path]) ->
   application:set_env(lager,handlers,[{lager_console_backend,debug}]),
   lager:start(),
 
+  {ok, _} = pulsedb:open(simple_db, [{url,"file://"++Path}]),
+
   Dispatch = [{'_', [
-    {"/api/v1/pulse_push", pulsedb_netpush_handler, [{path,Path}]}
+    {"/api/v1/pulse_push", pulsedb_netpush_handler, [{db,simple_db}] ++ Auth}
   ]}],
+
   {ok, L} = ranch:start_listener(fake_pulsedb, 1, ranch_tcp, [{port,Port}], cowboy_protocol, [{env, [
     {dispatch, cowboy_router:compile(Dispatch)}
   ]}]),
